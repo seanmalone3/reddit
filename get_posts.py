1fb70363f12f02be16b52495911d108f2af05cd8
seanmalone3 <seanmalone3@gmail.com>
2018-03-01 14:48:56 -0500

    added top post comment gatherer

--- get_posts.py
+++ get_posts.py
@@ -2,29 +2,25 @@
 # import requests.auth
 # from config import *
 # import json
-#
-# client_auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-# post_data = {"grant_type": "password", "username": username, "password": password}
-# headers = {"User-Agent": "DictionaryBot/0.1 by thewhiteone3"}
-# response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-# response.json()
-# res = json.loads(response.text)
-# token = res['access_token']
-# print(token)
 
 import praw
 from config import *
 from praw.models import MoreComments
+
 r = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      password=password,
                      user_agent=user_agent,
                      username=username)
-# print(reddit.user.me())
-posts = r.subreddit('science').top(limit=100)
-for x in posts:
-    print(x.score, x.title)
-
+posts = r.subreddit('science').top(limit=5)
+for post in posts:
+    submission = r.submission(id=post)
+    comment_count = 0
+    comment_sum = 0
+    for comment in submission.comments[0:4]:
+        comment_sum += comment.score
+        comment_count += 1
+    print('\nPost_title:', submission.title, '\nScore:', submission.score, 'Ratio:', submission.upvote_ratio, 'Comments:', submission.num_comments, 'Top Comment Average:', comment_sum/comment_count, '\nURL:', submission.url)
 
 # submission = r.submission(id='3g1jfi')
 #
